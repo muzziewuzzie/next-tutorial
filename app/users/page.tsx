e5b4282b84aa102113f553fba837b49c81478f06
Muzakkir Syed <musavvirmuzakkir@gmail.com>
2023-11-05 23:03:32 +0800

    routing

---
+++ app/users/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserTable from "./UserTable";
+
+const UsersPage = async () => {
+  return (
+    <>
+      <h1>Users</h1>
+      <UserTable />
+    </>
+  );
+};
+
+export default UsersPage;
